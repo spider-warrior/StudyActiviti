29863ba001cb35d54b82566429a93aaa042e2650
yangjian <362961910@qq.com>
2017-02-16 20:51:18 +0800

    HomeController添加controller注解

--- Activiti-Web/src/main/java/com/jy/activiti/controller/HomeController.java
+++ Activiti-Web/src/main/java/com/jy/activiti/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.jy.activiti.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@RestController
 @RequestMapping("/home")
 public class HomeController extends BaseController{
 
