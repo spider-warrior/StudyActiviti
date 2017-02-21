7fcc4da92d94dc723ea938da90bbb8478d0e4bb0
yangjian <362961910@qq.com>
2017-02-21 19:38:17 +0800

    添加以来解决图片文件不能保存问题

--- Activiti-Web/doc/db.sql
+++ Activiti-Web/doc/db.sql
@@ -1,8 +1,10 @@
 --1.
 CREATE USER 'activiti-test'@'localhost' IDENTIFIED BY 'activiti-test';
+CREATE USER 'activiti-test'@'%' IDENTIFIED BY 'activiti-test';
 
 --2.
 grant all privileges on *.* to 'activiti-test'@'localhost' identified by 'activiti-test';
+grant all privileges on *.* to 'activiti-test'@'%' identified by 'activiti-test';
 
 --3.
 flush privileges;
